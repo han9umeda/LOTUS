d655d18152ef91b4b7534ff981cec27e9f2c0ca8
Naoki Umeda <n-umeda@ist.osaka-u.ac.jp>
2021-12-28 12:30:26 +0900

    AS を登録して、登録内容を表示するフローを記述

--- main.py
+++ main.py
@@ -6,10 +6,24 @@ class AS_class_list:
     self.class_list = {}
 
   def add_AS(self, as_number):
-    self.class_list[as_number] = str(as_number)
+    self.class_list[as_number] = AS_class(as_number)
 
-  def get_AS_list(self):
-    return self.class_list
+  def show_AS_list(self):
+    for c in self.class_list.values():
+      c.show_info()
+
+class AS_class:
+  def __init__(self, asn):
+    self.as_number = asn
+    self.network_address = "xxxx"
+    self.routing_table = {}
+    self.policy = []
+
+  def show_info(self):
+    print(self.as_number)
+    print(self.network_address)
+    print(self.routing_table)
+    print(self.policy)
 
 class Interpreter(Cmd):
   intro = "=== This is ASPA simulator. ==="
@@ -22,7 +36,7 @@ class Interpreter(Cmd):
     as_class_list.add_AS(line)
 
   def do_showASList(self, line):
-    print(as_class_list.get_AS_list())
+    as_class_list.show_AS_list()
 
 as_class_list = AS_class_list()
 
