b098627bc4499e7fecb3cc7d1ecef89e1593c1b5
梅田 直希 <han9umeda@umedanoMacBook-Air.local>
2021-12-28 11:27:01 +0900

    AS_class_list を一旦実装

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from cmd import Cmd
 
+class AS_class_list:
+
+  def __init__(self):
+    self.class_list = {}
+
+  def add_AS(self, as_number):
+    self.class_list[as_number] = str(as_number)
+
+  def get_AS_list(self):
+    return self.class_list
+
 class Interpreter(Cmd):
   intro = "=== This is ASPA simulator. ==="
-  prompt = "aspa_simulation>> "
+  prompt = "aspa_simulation >> "
 
   def do_exit(self):
     return True
